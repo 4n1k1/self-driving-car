b7600e732819ced7426495a0e18e77523334ef9e
Artsiom Anikeyenka <arty.anikey@gmail.com>
2017-12-10 00:09:57 +0300

    Cars are moving and rotating around center properly

--- test.py
+++ test.py
@@ -3,29 +3,56 @@
 from kivy.app import App
 from kivy.vector import Vector
 from kivy.uix.widget import Widget
+from kivy.uix.relativelayout import RelativeLayout
+from kivy.graphics import Color
+from kivy.graphics.context_instructions import PushMatrix, PopMatrix, Rotate, Translate
 from kivy.clock import Clock
 
 _CARS_COUNT = 2
 
 
-class Car(Widget):
+class Car(RelativeLayout):
 	def __init__(self, car_idx):
-		self.color = (1, 1, 1, 1)
-		self.pos = Vector((car_idx + 1) * 5, (car_idx + 1) * 5)
-
-		Widget.__init__(self)
-
-		self.add_widget(Sensor(Vector(-7, -15) + self.pos, (1, 0, 0, 1)))
-		self.add_widget(Sensor(Vector(-15, 0) + self.pos, (0, 1, 0, 1)))
-		self.add_widget(Sensor(Vector(-7, 15) + self.pos, (0, 0, 1, 1)))
-
+		self.pos = Vector((car_idx + 1) * 20, (car_idx + 1) * 20)
 		self.velocity = Vector(car_idx + 1, 0)
+
+		RelativeLayout.__init__(self)
+
+		self.body = Body(Vector(-5, -5), (1, 1, 1, 1))
+		self.middle_sensor = Sensor(Vector(-20, -5), (1, 0, 0, 1))
+		self.right_sensor = Sensor(Vector(-12, 10), (0, 0, 1, 1))
+		self.left_sensor = Sensor(Vector(-12, -20), (0, 1, 0, 1))
+		self.center_mark = Center()
+
+		self.add_widget(self.body)
+		self.add_widget(self.middle_sensor)
+		self.add_widget(self.right_sensor)
+		self.add_widget(self.left_sensor)
+		self.add_widget(self.center_mark)
 
 	def move(self):
 		self.pos = self.velocity + self.pos
 
-		for widget in self.children:
-			widget.move()
+		with self.canvas.before:
+			PushMatrix()
+			r = Rotate()
+			r.angle = 5
+
+		with self.canvas.after:
+			PopMatrix()
+
+
+class Center(Widget):
+    pass
+
+
+class Body(Widget):
+	def __init__(self, pos, color):
+		self.color = color
+		self.pos = pos
+
+		Widget.__init__(self)
+
 
 
 class Sensor(Widget):
@@ -35,9 +62,9 @@ class Sensor(Widget):
 
 		Widget.__init__(self)
 
-	def move(self):
-		self.pos = self.parent.velocity + self.pos
-
+	@property
+	def abs_pos(self):
+		return self.parent.pos + self.pos
 
 class Map(Widget):
 
@@ -52,8 +79,8 @@ class Map(Widget):
 			self.add_widget(Car(i))
 
 	def update(self, dt):
-		for widget in self.children:
-			widget.move()
+		for car in self.children:
+			car.move()
 
 
 class CarApp(App):
